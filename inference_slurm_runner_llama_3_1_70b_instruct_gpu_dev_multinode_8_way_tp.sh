701f8b25241d75d75322edc1792dc58bae287185
Aflah <aflah20082@iiitd.ac.in>
2024-12-20 11:37:29 +0100

    70B New Configs

---
+++ inference_slurm_runner_llama_3_1_70b_instruct_gpu_dev_multinode_8_way_tp.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -l
+
+#SBATCH -o SLURM_Logs/%x_%j_%A-%T.out
+#SBATCH -e SLURM_Logs/%x_%j_%A-%T.err
+#SBATCH -D ./
+#SBATCH -J 70b-alpha
+
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=100GB
+
+#SBATCH --partition="gpu-bw"
+#SBATCH --gres=gpu:a100:4
+
+# Wall clock limit (max. is 24 hours):
+#SBATCH --time=01:00:00
+
+module load python-waterboa/2024.06 
+
+# Activate Env -
+
+source "/ptmp/afkhan/virtual_environments/vllm_env/bin/activate"
+
+# Define Variables -
+
+model="/ptmp/afkhan/Models/Llama-3.1-70B-Instruct"
+tp_size=8
+pp_size=1
+prompt_path="/ptmp/afkhan/vLLM-Serving-POC/Data/Prompt_OHB_Chat_Alpha.txt"
+save_path="/ptmp/afkhan/vLLM-Serving-POC/Data/Outputs_Llama-3.1-70B-Instruct_OHB_Chat_Alpha_tp_8_pp_1.json"
+queries_path="/ptmp/afkhan/vLLM-Serving-POC/Data/FAQ_en.csv"
+
+# Run Inference -
+
+echo "Running Inference"
+echo "Model: $model"
+echo "TP Size: $tp_size"
+echo "PP Size: $pp_size"
+echo "Save Path: $save_path"
+echo "Queries Path: $queries_path"
+
+python main.py --model $model --tp_size $tp_size --pp_size $pp_size --save_path $save_path --queries_path $queries_path --prompt_path $prompt_path
+#python main.py --model "/ptmp/afkhan/Models/pythia-14m" --tp_size 4 --pp_size 1 --save_path "/ptmp/afkhan/vLLM-Serving-POC/Data/Outputs_pythia-14m_OHB_Chat_Alpha_tp_4_pp_1.json" --queries_path "/ptmp/afkhan/vLLM-Serving-POC/Data/FAQ_en.csv" --prompt_path "/ptmp/afkhan/vLLM-Serving-POC/Data/Prompt_OHB_Chat_Alpha.txt"
+
+echo "Inference Done, Exiting"
+
